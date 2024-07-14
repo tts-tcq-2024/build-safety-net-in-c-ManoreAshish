58b28958a3f960bda59c95f4ae3010859c8369bd
ManoreAshish <manoreashish@gmail.com>
2024-07-14 23:34:12 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -5,17 +5,69 @@
 #include <ctype.h>
 #include <string.h>
 
-char getSoundexCode(char c) {
-    c = toupper(c);
-    switch (c) {
+char getSoundexCode(char test) {
+    test = toupper(test);
+    int Counter=0;
+    /*switch (c) {
         case 'B': case 'F': case 'P': case 'V': return '1';
         case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
         case 'D': case 'T': return '3';
         case 'L': return '4';
         case 'M': case 'N': return '5';
         case 'R': return '6';
-        default: return '0'; // For A, E, I, O, U, H, W, Y
+        default: return '0'; // For A, E, I, O, U, H, W, Y */
+
+    void isBFPV(char test)
+    {
+        if(test== 'B' || 'F' || 'P' || 'V')
+            return 1;
+        Counter++;
     }
+    if(Counter == 0)
+    {
+     void isCGJ (char test)
+        {
+            if(test== 'C')
+                return 2;
+            Counter++;
+        }
+    }
+    if(Counter == 0)
+    {
+        void isDT (char test)
+    { if(test=='D')
+        return 3;
+        Counter++;
+    }
+    }
+    if(Counter == 0)
+    {
+        isL (char test)
+        {
+          if(test=='L')  
+              return 4;
+            counter++;
+        }
+    }
+    if(Counter==0)
+    {
+        isMN(char test)
+        {
+            if(test == 'M')
+                return 5;
+            Counter++;
+        }
+    }
+    if(Counter ==0)
+    {
+        isR(char test)
+        {
+            if(test == 'R')
+                return 6;
+            Counter++;
+        }
+    }
+    else return 0;
 }
 
 void generateSoundex(const char *name, char *soundex) {
@@ -23,9 +75,11 @@ void generateSoundex(const char *name, char *soundex) {
     soundex[0] = toupper(name[0]);
     int sIndex = 1;
 
-    for (int i = 1; i < len && sIndex < 4; i++) {
+    for (int i = 1; i < len && sIndex < 4; i++) 
+    {
         char code = getSoundexCode(name[i]);
-        if (code != '0' && code != soundex[sIndex - 1]) {
+        if (code != '0' && code != soundex[sIndex - 1]) 
+        {
             soundex[sIndex++] = code;
         }
     }
