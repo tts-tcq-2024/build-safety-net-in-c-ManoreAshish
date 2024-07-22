d4cb30abf301ab9e8e1eb2c391ba91d9a31967e5
ManoreAshish <manoreashish@gmail.com>
2024-07-22 20:42:36 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -10,74 +10,58 @@ char getSoundexCode(char test)
 {
     test = toupper(test);
     int Result=0;
-    /*switch (c) {
-        case 'B': case 'F': case 'P': case 'V': return '1';
-        case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
-        case 'D': case 'T': return '3';
-        case 'L': return '4';
-        case 'M': case 'N': return '5';
-        case 'R': return '6';
-        default: return '0'; // For A, E, I, O, U, H, W, Y */
-    
     Result= isBFPV (test);
-    if(Counter == 0) Result=isCGJ (test);
-    if(Counter == 0) Result=isDT(test);
-    if(Counter == 0) Result= isL(test);
-    if(Counter==0) Result=isMN(test);
-    if(Counter==0) Result= isR(test);
-     else return 0;
 }   
     
 
-    int isBFPV(char test)
-    {
-        if(test== 'B' || 'F' || 'P' || 'V')
-            return 1;
-        Counter++;
-    }
+int isBFPV(char test)
+{
+ if(test== 'B' || 'F' || 'P' || 'V')
+ return 1;
+ else isCGJ(test);
+}
     
     
-     int isCGJ (char test)
-    {
-            if(test== 'C')
-                return 2;
-            Counter++;
-    }
+int isCGJ (char test)
+{
+ if(test== 'C')
+ return 2;
+ else isDT(test);
+}
   
-     int isDT (char test)
-    { if(test=='D')
-        return 3;
-        Counter++;
-    }
-    
-   
-    int  isL (char test)
-     {
-        if(test=='L')  
-        return 4;
-        Counter++;
-     }
-    
-    
-    
-        int isMN(char test)
-        {
-            if(test == 'M')
-                return 5;
-            Counter++;
-        }
+int isDT (char test)
+{ if(test=='D')
+  return 3;
+  else isL(test);
+}
 
-       int isR(char test)
-        {
-            if(test == 'R')
-                return 6;
-            Counter++;
-        }
+
+int  isL (char test)
+{
+ if(test=='L')  
+ return 4;
+ else isMN(test);
+}
+
+int isMN(char test)
+{
+ if(test == 'M')
+ return 5;
+ else isR(test);
+}
+
+ int isR(char test)
+{
+if(test == 'R')
+return 6;
+else return 0;
+}
     
    
 
 
-void generateSoundex(const char *name, char *soundex) {
+void generateSoundex(const char *name, char *soundex) 
+{
     int len = strlen(name);
     soundex[0] = toupper(name[0]);
     int sIndex = 1;
@@ -91,7 +75,8 @@ void generateSoundex(const char *name, char *soundex) {
         }
     }
 
-    while (sIndex < 4) {
+    while (sIndex < 4) 
+    {
         soundex[sIndex++] = '0';
     }
 
